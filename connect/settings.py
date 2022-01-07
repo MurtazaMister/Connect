45c627945da01e5a457d7925e5665fdb7c32c484
Murtaza M <murtazamister1@gmail.com>
2022-01-07 18:27:14 +0530

    Final

--- connect/settings.py
+++ connect/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = os.getenv('SECRET_KEY')
-with open('static/secretkey.txt') as f:
+with open(BASE_DIR / 'static/secretkey.txt') as f:
     SECRET_KEY = f.readline()
 
 # SECURITY WARNING: don't run with debug turned on in production!
