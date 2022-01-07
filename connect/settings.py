117411206ef0cb55af75fd28907d898843360e4a
Murtaza M <murtazamister1@gmail.com>
2022-01-07 18:36:53 +0530

    Final

--- connect/settings.py
+++ connect/settings.py
@@ -21,13 +21,12 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = os.getenv('SECRET_KEY')
-with open('secretkey.txt') as f:
+with open(BASE_DIR / 'secretkey.txt') as f:
     SECRET_KEY = f.readline()
 
 # SECURITY WARNING: don't run with debug turned on in production!
