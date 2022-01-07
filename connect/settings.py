71059ad28bf0d18907408a8f310e8d0106ebd97b
Murtaza M <murtazamister1@gmail.com>
2022-01-07 18:40:56 +0530

    Final

--- connect/settings.py
+++ connect/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.getenv('SECRET_KEY')
-with open(BASE_DIR / 'secretkey.txt') as f:
-    SECRET_KEY = f.readline()
+SECRET_KEY = os.getenv('SECRET_KEY')
+# with open(BASE_DIR / 'secretkey.txt') as f:
+#     SECRET_KEY = f.readline()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
